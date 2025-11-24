213e7a4fe626b744f7f417c593600e8fc7792ab6
kimi <kimi@sanskartechnolab.com>
2025-11-24 18:26:35 +0530

    overrirde logic in delivery trip commit

--- vehicle_tracking/__init__.py
+++ vehicle_tracking/__init__.py
@@ -1,1 +1,7 @@
 __version__ = "0.0.1"
+
+
+from erpnext.stock.doctype.delivery_trip.delivery_trip import DeliveryTrip
+from vehicle_tracking.public.py.delivery_trip import custom_validate_stop_addresses
+
+DeliveryTrip.validate_stop_addresses  = custom_validate_stop_addresses
